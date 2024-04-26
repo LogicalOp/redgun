c6d385b87b6140ad6c3053c16c73600261e77a0f
LogicalOp <xdylar@gmail.com>
2024-04-26 19:46:59 +0100

    Update some basic structure

---
+++ frontend/src/pages/Course.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// "Boilerplate" Course page, swap contents based on selected course
+
+const Course = () => {
+    return (
+        <div>
+            <h1>Course</h1>
+        </div>
+    );
+};
+
+export default Course;
