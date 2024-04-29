beb86175edec3f6e6a96f7f05f0cf4be14c612e2
dylan <xdylar@gmail.com>
2024-04-29 14:34:52 +0100

    Remove authChecks for now.

--- frontend/src/pages/Course.js
+++ frontend/src/pages/Course.js
@@ -4,9 +4,7 @@ import { isAuthenticated } from '../utils/auth';
 // "Boilerplate" Course page, swap contents based on selected course
 
 const Course = () => {
-    if(!isAuthenticated()) {
-        window.location.href = '/login';
-    }
+    
     return (
         <div>
             <h1>Course</h1>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { isAuthenticated } from '../utils/auth';
 
 const Home = () => {
-    if(!isAuthenticated()) {
-        window.location.href = '/login';
-    }
+    
     return (
         <div>
             <h1>Home</h1>
