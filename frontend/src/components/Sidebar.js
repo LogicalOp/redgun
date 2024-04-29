a5f05bcd3e1704709fe14116a9db5dd2d22657b9
dylan <xdylar@gmail.com>
2024-04-29 14:57:10 +0100

    Use useNavigate hook instead of window.href.

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import { SideNavigation, SideNavigationItem, SideNavigationSubItem } from "@ui5/webcomponents-react";
+import { useLocation, useNavigate } from 'react-router-dom';
 import React from "react";
 
 const routes = {
@@ -8,7 +9,8 @@ const routes = {
 };
 
 const Sidebar = () => {
-  const currentPath = window.location.pathname;
+  const navigate = useNavigate();
+  const currentPath = useLocation().pathname;
 
   return (
     <SideNavigation style={{ position: 'fixed', left: 0, top: 0, bottom: 0 }}>
@@ -17,7 +19,7 @@ const Sidebar = () => {
           key={route}
           text={route}
           selected={routes[route] === currentPath}
-          onClick={() => window.location.href = routes[route]}
+          onClick={() => navigate(routes[route])}
         />
       ))}
     </SideNavigation>
