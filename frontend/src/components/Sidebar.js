27120fa2e36187ad19e8a874e590bb6b3727fbd3
dylan <xdylar@gmail.com>
2024-04-29 14:55:49 +0100

    Update sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,25 +1,27 @@
 import { SideNavigation, SideNavigationItem, SideNavigationSubItem } from "@ui5/webcomponents-react";
 import React from "react";
- 
+
+const routes = {
+  "Home": "/",
+  "Learning": "/learning",
+  // Add more routes as needed
+};
+
 const Sidebar = () => {
+  const currentPath = window.location.pathname;
+
   return (
-   
-      <SideNavigation style={{ position: 'fixed', left: 0, top: 0, bottom: 0 }}
-        onSelectionChange={function _a() {}}
-      >
-        <SideNavigationItem icon="home" text="Home" />
-        <SideNavigationItem icon="email" text="Learning" />
-        <SideNavigationItem expanded icon="group" text="People">
-          <SideNavigationSubItem text="From My Team" />
-          <SideNavigationSubItem text="From Other Teams" />
-        </SideNavigationItem>
-        <SideNavigationItem icon="locate-me" selected text="Locations" />
-        <SideNavigationItem icon="calendar" text="Events">
-          <SideNavigationSubItem text="Local" />
-          <SideNavigationSubItem text="Others" />
-        </SideNavigationItem>
-      </SideNavigation>
-   
+    <SideNavigation style={{ position: 'fixed', left: 0, top: 0, bottom: 0 }}>
+      {Object.keys(routes).map((route) => (
+        <SideNavigationItem
+          key={route}
+          text={route}
+          selected={routes[route] === currentPath}
+          onClick={() => window.location.href = routes[route]}
+        />
+      ))}
+    </SideNavigation>
   );
 };
+
 export default Sidebar;
