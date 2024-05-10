9d17731d6120261717b2a9c135677f9b03fab71f
dylan <xdylar@gmail.com>
2024-05-10 10:22:57 +0100

    Remove unused imports in Login.js.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import LoginForm from '../components/Forms/LoginForm';
-import { useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    
+
     return (
         <div>
             <LoginForm />
